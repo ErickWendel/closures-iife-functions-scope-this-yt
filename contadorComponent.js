e1547ec04515ec82fef3182fd9074e9466a2b17b
Erick Wendel <erick.workspace@gmail.com>
2020-12-30 17:09:46 -0300

    demos
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

---
+++ contadorComponent.js
@@ -0,0 +1,96 @@
+// Immediately Invoked Function Expression ou IFFE
+// também é uma closure
+// encapsulamos todo o nosso código em uma funçao e expomos somente via window, para
+// nao ter variavel global perdida
+
+(() => {
+    const BTN_REINICIAR = 'btnReiniciar'
+    const ID_CONTADOR = 'contador'
+    const VALOR_CONTADOR = 100
+    const PERIODO_INTERVALO = 10
+
+    class ContadorComponent {
+        constructor() {
+            this.iniciar()
+        }
+
+        atualizarTexto = ({ elementoContador, contador }) => () => {
+            const identificadorNoTexto = "$$contador"
+            const textoPadrao = `Começando em <strong>${identificadorNoTexto}</strong> segundos...`
+            elementoContador.innerHTML = textoPadrao.replace(identificadorNoTexto, contador.valor--)
+        }
+
+        agendarParadaContador({ elementoContador, idDoIntervalo }) {
+            return () => {
+                clearInterval(idDoIntervalo)
+                // deixamos sem texto
+                elementoContador.innerHTML = ""
+                this.desabilitarBotao(false)
+            }
+        }
+
+        prepararBotao(elementoBotao, iniciarFn) {
+            elementoBotao.addEventListener('click', iniciarFn.bind(this))
+
+            return (valor = true) => {
+                const atributo = 'disabled'
+
+                if (valor) {
+                    elementoBotao.setAttribute(atributo, valor)
+                    return;
+                }
+
+                elementoBotao.removeAttribute(atributo)
+
+            }
+        }
+
+        prepararContadorProxy() {
+            const handler = {
+                set: (currentContext, propertyKey, newValue) => {
+                    console.log(currentContext, propertyKey, newValue)
+
+                    if (!currentContext.valor) {
+                        currentContext.efetuarParada()
+                    }
+
+                    currentContext[propertyKey] = newValue
+                    return true
+                },
+            }
+
+            const contador = new Proxy({
+                valor: VALOR_CONTADOR,
+                efetuarParada: () => { },
+            }, handler)
+
+            return contador
+        }
+
+        iniciar() {
+            console.log('iniciou!')
+            const elementoBotao = document.getElementById(BTN_REINICIAR)
+            const desabilitarBotao = this.prepararBotao(elementoBotao, this.iniciar)
+            desabilitarBotao()
+
+            const executarEmCada = PERIODO_INTERVALO
+            const elementoContador = document.getElementById(ID_CONTADOR)
+
+            const contador = this.prepararContadorProxy()
+            const argumentos = { elementoContador, contador }
+
+            const fn = this.atualizarTexto(argumentos)
+            const idDoIntervalo = setInterval(fn, executarEmCada)
+
+
+            {
+                const argumentos = { elementoContador, idDoIntervalo }
+                const pararContadorFn = this.agendarParadaContador.apply({ desabilitarBotao }, [argumentos])
+                contador.efetuarParada = pararContadorFn
+            }
+        }
+
+    }
+
+    window.ContadorComponent = ContadorComponent
+})()
